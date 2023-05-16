47b2e2ff359ffe8e383c4e4c84908a682896e805
KyleRiggen <kyle.riggenbach@gmail.com>
2023-05-16 15:11:41 -0600

    decent template

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const homeRoutes = require('./routes/home');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use(homeRoutes);
 
 app.use((req, res, next) => {
     res.status(404).send('<h1>page not found</h1>')
--- routes/home.js
+++ routes/home.js
@@ -7,8 +7,8 @@ const rootDir = require('../util/path')
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    //res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    //res.sendFile(path.join(__dirname, '..', 'views', 'home.html'));
+    res.sendFile(path.join(rootDir, 'views', 'index.html'));
 });
 
 module.exports = router;
