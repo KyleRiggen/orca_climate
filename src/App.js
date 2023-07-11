af835fba667ea78e8e81ab972fa6a1c4b82ed384
KyleRiggen <kyle.riggenbach@gmail.com>
2023-07-11 03:23:18 -0600

    restart on new project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+function App() {
+  return (
+    <div>
+      <div>
+        <section className="nav">
+          <a></a>
+          <div>
+            <a>menu item1</a>
+            <a>menu item2</a>
+            <a>menu item3</a>
+            <a>menu item4</a>
+          </div>
+        </section>
+      </div>
+      <p>just one thing</p>
+    </div>
+  );
+}
+
+export default App;
